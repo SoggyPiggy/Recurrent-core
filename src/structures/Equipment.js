087cd922a8e11770dc2da39cbbffa1680049878b
SoggyPiggy <soggypiggy@gmail.com>
2018-08-14 23:59:46 -0400

    Added a Equipment class based off of items for player gear

---
+++ src/structures/Equipment.js
@@ -0,0 +1,21 @@
+const { Item } = require('./Item');
+const { Attributes } = require('./Attributes');
+
+class Equipment extends Item
+{
+	// TODO: Also change the something here, just like in Item class
+	constructor(something, data = {})
+	{
+		super(something, data);
+		this.attributes = new Attributes(data.attributes);
+	}
+
+	toJSON()
+	{
+		const data = super.toJSON();
+		data.attributes = this.attributes.toJSON();
+		return data;
+	}
+}
+
+module.exports = { Equipment };
