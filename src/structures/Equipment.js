58183c853219c9a7383a51a1196ffc5e46b04afe
SoggyPiggy <soggypiggy@gmail.com>
2018-08-15 02:46:29 -0400

    Added a static method to create a blank/empty Equipment item

--- src/structures/Equipment.js
+++ src/structures/Equipment.js
@@ -7,14 +7,26 @@ class Equipment extends Item
 	constructor(data = {})
 	{
 		super(data);
+		this.empty = typeof data.empty !== 'undefined' ? data.empty : false;
 		this.attributes = new Attributes(data.attributes);
 	}
 
 	toJSON()
 	{
 		const data = super.toJSON();
+		data.empty = this.empty;
 		data.attributes = this.attributes.toJSON();
 		return data;
+	}
+
+	static empty()
+	{
+		const data = {};
+		data.empty = true;
+		data.name = 'Empty';
+		data.description = null;
+		data.attributes = Attributes.template();
+		return new Equipment(data);
 	}
 }
 
