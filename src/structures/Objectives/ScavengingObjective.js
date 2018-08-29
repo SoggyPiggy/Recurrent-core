fd1257c84405093b1b8f63061d61f16e9c075f25
SoggyPiggy <soggypiggy@gmail.com>
2018-08-28 20:42:24 -0400

    Add Scavenge Objective

---
+++ src/structures/Objectives/ScavengingObjective.js
@@ -0,0 +1,26 @@
+const { Objective } = require('../Objective');
+
+class ScavengingObjective extends Objective
+{
+	constructor(chapter, data)
+	{
+		super(chapter, data);
+		this.type = 'scavenge';
+	}
+
+	generateRewards()
+	{
+		const xp = this.generateXPReward();
+		return { xp };
+	}
+
+	advance()
+	{
+		let progress = super.advance();
+		progress *= (1 + (this.player.ingenutiyMod + this.player.awarenessMod) / 2);
+		if (this.player.status.fatigued) progress *= 0.8;
+		return progress;
+	}
+}
+
+module.exports = { ScavengingObjective };
