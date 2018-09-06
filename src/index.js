12469b35620ce26344b2e212564059e2ba2e1ff9
SoggyPiggy <soggypiggy@gmail.com>
2018-09-06 13:40:36 -0400

    Add all base structures to index exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 const { Game } = require('./structures/Game');
 
-module.exports = { Game };
+const { Attributes } = require('./structures/Attributes');
+const {
+	Base,
+	EventBase,
+	ArrayBase,
+	ManagerBase,
+	TickBase,
+} = require('./structures/Bases');
+const { Chapter } = require('./structures/Chapter');
+const { Coordinates } = require('./structures/Coordinates');
+const { Equipment } = require('./structures/Equipment');
+const { Experience } = require('./structures/Experience');
+const { Item } = require('./structures/Item');
+const { Objective } = require('./structures/Objective');
+const { Player } = require('./structures/Player');
+const { Quest } = require('./structures/Quest');
+const { Storage } = require('./structures/Storage');
+
+module.exports = {
+	Game,
+
+	Base,
+	EventBase,
+	ArrayBase,
+	ManagerBase,
+	TickBase,
+
+	Attributes,
+	Chapter,
+	Coordinates,
+	Equipment,
+	Experience,
+	Item,
+	Objective,
+	Player,
+	Quest,
+	Storage,
+};
