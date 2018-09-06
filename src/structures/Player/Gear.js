983636363378b0f6d0975b8a8c0bc53ad2ff3e0e
SoggyPiggy <soggypiggy@gmail.com>
2018-09-06 12:11:24 -0400

    Update Gear Gears slots are now getters/setters Everything now is generated from a single static getter of array

--- src/structures/Player/Gear.js
+++ src/structures/Player/Gear.js
@@ -1,5 +1,5 @@
 const { Base } = require('../Bases');
-const { GearSlot } = require('./Gear/Slot');
+const { Equipment } = require('./../Equipment');
 const { GearStorage } = require('./Gear/Storage');
 
 class PlayerGear extends Base
@@ -8,24 +8,74 @@ class PlayerGear extends Base
 	{
 		super();
 		this.player = player;
-		this.head = new GearSlot(this, data.head);
-		this.body = new GearSlot(this, data.body);
-		this.legs = new GearSlot(this, data.legs);
-		this.hands = new GearSlot(this, data.hands);
-		this.waist = new GearSlot(this, data.waist);
+		PlayerGear.slots.forEach((part) =>
+		{
+			this[`${part}ID`] = typeof data[`${part}ID`] !== 'undefined' ? data[`${part}ID`] : null;
+		});
 		this.storage = new GearStorage(this, data.storage);
+		this.refreshReferences();
+		this.setupGettersSetters();
+	}
+
+	setupGettersSetters()
+	{
+		PlayerGear.slots.forEach((part) =>
+		{
+			Object.defineProperty(this, part, {
+				get: () => this[`${part}Reference`],
+				set: (item) =>
+				{
+					if (item instanceof Equipment)
+					{
+						this[`${part}ID`] = item.id;
+						this[`${part}Reference`] = item;
+					}
+					else
+					{
+						this[`${part}ID`] = null;
+						this[`${part}Reference`] = Equipment.empty();
+					}
+				},
+			});
+		});
+	}
+
+	refreshReferences()
+	{
+		PlayerGear.slots.forEach((part) =>
+		{
+			const id = this[`${part}ID`];
+			if (id !== null)
+			{
+				const equipment = this.storage.find(item => item.id === id);
+				if (equipment)
+				{
+					this[`${part}Reference`] = equipment;
+					return;
+				}
+			}
+			this[`${part}Reference`] = Equipment.empty();
+		});
 	}
 
 	jsonKeys()
 	{
 		return [
 			...super.jsonKeys(),
+			...PlayerGear.slots.map(part => `${part}ID`),
+			'storage',
+		];
+	}
+
+	static get slots()
+	{
+		return [
 			'head',
 			'body',
 			'legs',
+			'feet',
 			'hands',
 			'waist',
-			'storage',
 		];
 	}
 }
