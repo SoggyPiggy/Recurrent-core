b6c092e74bd2c351458d69700d2c07c3100cea57
SoggyPiggy <soggypiggy@gmail.com>
2018-09-07 21:47:28 -0400

    Add functionality to SaveManager

--- src/managers/SaveManager.js
+++ src/managers/SaveManager.js
@@ -1,4 +1,11 @@
+const hashsum = require('hash-sum');
 const { EventEmitter } = require('events');
+const {
+	Game,
+	Chapter,
+	Quest,
+	Item,
+} = require('./../index');
 
 class SaveManager extends EventEmitter
 {
@@ -10,6 +17,9 @@ class SaveManager extends EventEmitter
 		this.ticked = new Set();
 		this.hashes = new Map();
 		this.interval = setInterval(() => this.save(false), 120000);
+		this.game.on('chapterTick', chapter => this.ticked.add(chapter));
+		this.game.on('questTick', quest => this.ticked.add(quest));
+		this.game.on('playerTick', player => this.ticked.add(player));
 	}
 
 	save(force = true)
@@ -17,10 +27,16 @@ class SaveManager extends EventEmitter
 		const time = new Date();
 		if (force || (time - this.last > this.delay))
 		{
-			/* Example
-			const data = {};
-			this.emit('save', data, 'chapter', 'chapter-id');
-			*/
+			Array.from(this.ticked.entries()).map(item => item.compress()).forEach((item) =>
+			{
+				const hash = hashsum(item);
+				if (this.hashes.has(item.id) && this.hashes.get(item.id) === hash) return;
+				this.hashes.set(item.id, hash);
+				if (item instanceof Quest) this.emit('save', 'quest', item);
+				else if (item instanceof Chapter) this.emit('save', 'chapter', item);
+				else if (item instanceof Game) this.emit('save', 'game', item);
+			});
+			this.ticked.clear();
 		}
 	}
 }
