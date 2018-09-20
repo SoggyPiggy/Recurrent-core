13ad912997c33dd15a834394b2eb5ac8550471cb
SoggyPiggy <soggypiggy@gmail.com>
2018-09-20 12:34:58 -0400

    Add elf race

---
+++ src/resources/races/elf.js
@@ -0,0 +1,29 @@
+// cSpell:disable
+const { Race } = require('./../../structures/Race');
+
+module.exports = new Race({
+	id: 'elf',
+	title: 'Elven',
+	name: [
+		'Athtar',
+		'Cyran',
+		'Kesefehon',
+		'Taeral',
+		'Hagduin',
+		'Rhangyl',
+		'Kiirion',
+		'Almer',
+		'Vulmon',
+		'Avourel',
+		'Gaelira',
+		'Yrthraethra',
+		'Eloimaya',
+		'Nei',
+		'Alenia',
+		'Ashera',
+		'Alenia',
+		'Martainn',
+		'Aravae',
+		'Myriani',
+	],
+});
