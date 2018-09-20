0c1936489a40ab56e336f579e4ad3008675659b2
SoggyPiggy <soggypiggy@gmail.com>
2018-09-20 12:33:39 -0400

    Add orc race

---
+++ src/resources/races/orc.js
@@ -0,0 +1,19 @@
+// cSpell:disable
+const { Race } = require('./../../structures/Race');
+
+module.exports = new Race({
+	id: 'orc',
+	title: 'Orc',
+	name: [
+		'Xurek',
+		'Meakgu',
+		'Mug',
+		'Varbu',
+		'Dakgu',
+		'Shagar',
+		'Bum',
+		'Murob',
+		'Shazgob',
+		'Badbog',
+	],
+});
