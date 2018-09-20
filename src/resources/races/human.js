2074a6e8ba3f6301a5b9acf8df2aa58cdde722ce
SoggyPiggy <soggypiggy@gmail.com>
2018-09-20 12:32:56 -0400

    Add human race

---
+++ src/resources/races/human.js
@@ -0,0 +1,20 @@
+// cSpell:disable
+const { Race } = require('./../../structures/Race');
+
+module.exports = new Race({
+	id: 'human',
+	title: 'Human',
+	names: [
+		'Frut',
+		'Brar',
+		'Crilk',
+		'Dhak',
+		'Bhob',
+		'Thulga',
+		'Virthi',
+		'Amgai',
+		'Mirta',
+		'Feto',
+	],
+
+});
