80e9896944f9ea891b45f5c554a9106c85e26c88
SoggyPiggy <soggypiggy@gmail.com>
2018-09-23 03:43:28 -0400

    Update Race Added things for the heights

--- src/structures/Race.js
+++ src/structures/Race.js
@@ -8,11 +8,20 @@ class Race extends Base
 		this.id = data.id;
 		this.title = data.title;
 		this.names = data.names;
+		this.minHeight = data.minHeight;
+		this.maxHeight = data.maxHeight;
 	}
 
 	randomName()
 	{
 		return this.random.pick(this.names);
+	}
+
+	randomHeight()
+	{
+		let height = this.random.real(this.minHeight, this.maxHeight);
+		height += this.random.real(this.minHeight, this.maxHeight) * 0.5;
+		return Math.round(height / 1.5);
 	}
 
 	toString()
